09d0d7e0cab05261814c168192d0d3c2c3b7c0b1
nomissbowling <nomissbowling@yahoo.co.jp>
2018-06-06 19:08:09 +0900

    add RouletteController.cs

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,22 @@
+﻿/*
+  RouletteController.cs (Unity)
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RouletteController : MonoBehaviour {
+  float rotSpeed = 0.0f;
+
+  void Start(){
+  }
+
+  void Update(){
+    if(Input.GetMouseButtonDown(0)){
+      rotSpeed = 10.0f;
+    }
+    transform.Rotate(0, 0, rotSpeed);
+  }
+}

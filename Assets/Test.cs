b7d135e83ae4d45b6fe6f775bc38143f86e937fa
nomissbowling <nomissbowling@yahoo.co.jp>
2018-06-06 13:55:00 +0900

    modify Test.cs

---
+++ Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿/*
+  Test.cs (Unity)
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Test : MonoBehaviour {
+  int cnt;
+
+  // Use this for initialization
+  void Start(){
+    Debug.Log("Hello, world! 日本語UTF8");
+    cnt = 0;
+  }
+
+  // Update is called once per frame
+  void Update(){
+    string s = "日本語";
+    ++cnt;
+    if(cnt % 60 == 0) Debug.Log(s + cnt);
+  }
+}
